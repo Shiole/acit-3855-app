cea8f895d253f297321554e82937ca1ddeb36f5b
Kitty Laptop <kitty98low@gmail.com>
2023-11-22 19:13:38 -0800

    time

--- dashboard-ui/src/components/Health.js
+++ dashboard-ui/src/components/Health.js
@@ -52,7 +52,7 @@ export default function Health() {
 						</tr>
 					</tbody>
                 </table>
-                <h3>Last Updated: {health['last_updated']}</h3>
+                <h3>Last Updated: {health['last_updated'].slice(10)}</h3>
             </div>
         )
     }
