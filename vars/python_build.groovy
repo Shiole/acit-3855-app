6174189d3be10b8e1bf6b97ce4586538d381660c
Kitty Laptop <kitty98low@gmail.com>
2023-11-30 11:20:36 -0800

    groovy

---
+++ vars/python_build.groovy
@@ -0,0 +1,42 @@
+def call(dockerRepoName, imageName, portNum) {
+    pipeline {
+        agent any
+        parameters {
+            booleanParam(defaultValue: false, description: 'Deploy the App', name: 'DEPLOY')
+        }
+        stages {
+            stage("Lint") {
+                steps {
+                    sh "pylint --fail-under=5.0 ./${dockerRepoName}/*.py"
+                }
+            }
+            stage("Security") {
+                steps {
+                    sh "safety check -r ${dockerRepoName}/requirements.txt --full-report -o text --continue-on-error"
+                }
+            }
+            stage("Package") {
+            when {
+                expression { env.GIT_BRANCH == 'origin/main' }
+                    }
+                    steps {
+                    withCredentials([string(credentialsId: 'DockerHub', variable: 'TOKEN')]) {
+                        sh "docker login -u 'shiole' -p '$TOKEN' docker.io"
+                        sh "docker build -t ${dockerRepoName}:latest --tag shiole/${dockerRepoName}:${imageName} ."
+                        sh "docker push shiole/${dockerRepoName}:${imageName}"
+                    }
+                }
+            }
+            stage("Deploy") {
+                when {
+                    expression { params.DEPLOY } 
+                }
+                steps {
+                    sshagent(withCredentials([string(credentialsId: 'Kifka', variable: 'TOKEN')])) {
+                        sh "ssh -o StrictHostKeyChecking=no azureuser@20.63.111.22 'cd ~/acit-3855-kafka/acit-3855-app/deployment && docker pull shiole/${dockerRepoName}:latest && docker-compose up -d'"
+                    }
+                }
+            }
+        }
+    }
+}
